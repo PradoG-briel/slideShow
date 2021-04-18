971eef8235eed02a51fa1b38fe28406676c68644
PradoG-briel <gabriel.prado429@gmail.com>
2021-04-18 19:41:04 -0300

    slide show em js, com indicador de slide

--- slideShow.js
+++ slideShow.js
@@ -11,29 +11,30 @@ const images = [
 
 const containerItems = document.querySelector('#container-items');
 
-const loadImages = ( images, container ) => {
-    images.forEach ( image => {
-        container.innerHTML += `
-            <div class='item'>
-                <img src='${image.url}'
-            </div>
-        `
-    })
+const loadImages = (images, container ) => {
+    images.forEach (Image => {
+        container.innerHTML += ` 
+                <div class='item'>
+                    <img src='${image.url}'
+                </div>
+            `
+        })
 }
 
-loadImages( images, containerItems );
 
-let items = document.querySelectorAll('.item');
+loadImages(images, containerItems);
 
-const previous = () => {
-    containerItems.appendChild(items[0]);
-    items = document.querySelectorAll('.item');
+let items = document.querySelectorAll('.item'); //?
+
+const previous = ( ) => {
+    containerItems.appendChild(items[0]);//appendChild?
+    items = document.querySelectorAll ('.item');
 }
 
-const next = () => {
+const next = () =>{
     const lastItem = items[items.length - 1];
-    containerItems.insertBefore( lastItem, items[0] );
-    items = document.querySelectorAll('.item');
+    containerItems.insertBefore( lastItem, items[0]);
+    items = document.querySelectorAll ('.item');
 }
 
 document.querySelector('#previous').addEventListener('click', previous);
